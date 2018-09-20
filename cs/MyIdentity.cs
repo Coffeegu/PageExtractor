44e25b638a206288c9f91de184598ae32a962106
Coffeegu <42864543+Coffeegu@users.noreply.github.com>
2018-09-20 18:37:03 +0800

    Add files via upload

---
+++ cs/MyIdentity.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1.cs
+{
+    public class MyIdentity:System.Security.Principal.IIdentity
+    {
+        private string userID;
+        private string password;
+
+        public MyIdentity(string currentUserID, string currentPassword)
+        {
+            // 
+            // TODO: 在此处添加构造函数逻辑 
+            // 
+            userID = currentUserID;
+            password = currentPassword;
+        }
+
+        private bool CanPass()
+        {
+            //这里朋友们可以根据自己的需要改为从数据库中验证用户名和密码， 
+            //这里为了方便我直接指定的字符串 
+            SqlConnection con = new SqlConnection("server=.;database=Test;uid=sa;pwd=GZMgzm123");
+            using (SqlCommand sc = new SqlCommand())
+            {
+                string sql = "";
+                con.Open();
+                sc.Connection = con;
+                sc.CommandType = CommandType.Text;
+                SqlDataReader sr = null;
+                if (!string.IsNullOrEmpty(userID) && !string.IsNullOrEmpty(password))
+                {
+                    sql="select UserPassword from[User] where UserName='"+userID+"'";
+                    sc.CommandText = sql;
+                    sr = sc.ExecuteReader();
+                    if (sr.Read())
+                    {
+                        if (password == Convert.ToString(sr[0]))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            
+    }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        #region IIdentity 成员 
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                // TODO:   添加 MyIdentity.IsAuthenticated getter 实现 
+                return CanPass();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                // TODO:   添加 MyIdentity.Name getter 实现 
+                return userID;
+            }
+        }
+
+        //这个属性我们可以根据自己的需要来灵活使用,在本例中没有用到它 
+        public string AuthenticationType
+        {
+            get
+            {
+                // TODO:   添加 MyIdentity.AuthenticationType getter 实现 
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ cs/MyPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.cs
+{
+    public class MyPage : System.Web.UI.Page
+    {
+        public MyPage()
+        {
+            // 
+            // TODO: 在此处添加构造函数逻辑 
+            // 
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            this.Load += new EventHandler(MyPage_Load);
+        }
+
+        //在页面加载的时候从缓存中提取用户信息 
+        private void MyPage_Load(object sender, System.EventArgs e)
+        {
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                if (Context.Cache["UserMessage"] != null)
+                {
+                    Hashtable userMessage = (Hashtable)Context.Cache["UserMessage"];
+                    MyPrincipal principal = new MyPrincipal(userMessage["UserID"].ToString(), userMessage["UserPassword"].ToString());
+                    Context.User = principal;
+                }
+            }
+        }
+    }
+}
